c4c911d261898970b5f5825c47499672b6e4829c
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-10-29 21:26:45 +0530

    GUi for Repo - Full page careousel - In progress

--- js/page.js
+++ js/page.js
@@ -1,6 +1,21 @@
 /**
  * Created by suren on 29/10/14.
  */
-$('.carousel').carousel({
-    interval: 3000 //changes the speed
-})
+
+$(document).ready(function(){
+    $("#carousel").carousel({
+        interval: 6000 //changes the speed
+    });
+    $("#prev").click(function(){
+        $("#carousel").carousel('prev');
+    });
+//    $("#pause").click(function(){
+//        $("#carousel").carousel('pause');
+//    });
+//    $("#resume").click(function(){
+//        $("#carousel").carousel('cycle');
+//    });
+    $("#next").click(function(){
+        $("#carousel").carousel('next');
+    });
+});
