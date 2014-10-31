1c89bb0dbfa0e9204ebaf7c26963dc602c7715b2
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-10-31 20:56:35 +0530

    Careousel Template Complete

--- js/page.js
+++ js/page.js
@@ -4,17 +4,17 @@
 
 $(document).ready(function(){
     $("#carousel").carousel({
-        interval: 6000 //changes the speed
+        interval: 3000 //changes the speed
     });
     $("#prev").click(function(){
         $("#carousel").carousel('prev');
     });
-//    $("#pause").click(function(){
-//        $("#carousel").carousel('pause');
-//    });
-//    $("#resume").click(function(){
-//        $("#carousel").carousel('cycle');
-//    });
+    $("#pause").click(function(){
+        $("#carousel").carousel('pause');
+    });
+    $("#cycle").click(function(){
+        $("#carousel").carousel('cycle');
+    });
     $("#next").click(function(){
         $("#carousel").carousel('next');
     });
